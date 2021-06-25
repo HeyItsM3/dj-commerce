bf54663d897e4d531eef29412fdde93e2dbc1a87
Aldo <aldoyedro181@gmail.com>
2021-06-25 20:16:36 -0300

    New styles, Favicon, and Decouple

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h7ljwx%!__^rth%^1)e78mgm8w=k%qm-(3k-3%ikrk^r(5!l(o'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
